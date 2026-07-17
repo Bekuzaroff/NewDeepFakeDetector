b0d018891115c09e61d28ee9f4489845e31be01e
= <bekuzarovchermen@gmail.com>
2026-07-17 21:04:09 +0300

    Commit: db scheme

--- server/db/db.py
+++ server/db/db.py
@@ -20,13 +20,45 @@ class User(db.Model):
     def __repr__(self):
         return f"<User {self.username}>"
 
-class Images(db.Model):
-    __tablename__ = "images" 
-    img_id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"))
-    img_link: Mapped[str] = mapped_column(nullable=False)
-    img_size: Mapped[int] = mapped_column(nullable=False)
-    img_type: Mapped[str] = mapped_column(nullable=False)
+class Profession(db.Model):
+    __tablename__ = "professions" 
+    profession_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    desc: Mapped[str] = mapped_column(nullable=False)
+    def __repr__(self):
+        return f"<Profession {self.profession_id}>"
+
+class Topic(db.Model):
+    __tablename__ = "topics" 
+    topic_id: Mapped[int] = mapped_column(primary_key=True)
+    prof_id: Mapped[int] = mapped_column(ForeignKey("professions.profession_id"))
+    name: Mapped[str] = mapped_column(nullable=False)
 
     def __repr__(self):
-        return f"<Image {self.img_link}>"
+        return f"<Topic {self.topic_id}>"
+
+class Lesson(db.Model):
+    __tablename__ = "lessons" 
+    lesson_id: Mapped[int] = mapped_column(primary_key=True)
+    topic_id: Mapped[int] = mapped_column(ForeignKey("topics.topic_id"))
+    name: Mapped[str] = mapped_column(nullable=False)
+
+    def __repr__(self):
+        return f"<Lesson {self.lesson_id}>"
+
+class Exercise(db.Model):
+    __tablename__ = "exercises" 
+    exercise_id: Mapped[int] = mapped_column(primary_key=True)
+    lesson_id: Mapped[int] = mapped_column(ForeignKey("lessons.lesson_id"))
+    name: Mapped[str] = mapped_column(nullable=False)
+
+    def __repr__(self):
+        return f"<Exercise {self.exercise_id}>"
+
+class UsersProfessions(db.Model):
+    __tablename__ = "users_professions" 
+    profession_id: Mapped[int] = mapped_column(ForeignKey("professions.profession_id"), primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"), primary_key=True)
+    def __repr__(self):
+        return f"<Profession {self.profession_id} of User {self.user_id}>"
+
